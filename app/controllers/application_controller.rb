0292eff559c5b5c5f779c55e0babeb273f15943b
takaratomoya <tomoya48698103@gmail.com>
2019-07-13 17:03:15 +0900

    chris

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::Base
+
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
 	protected
 
-	def configue_permitted_parameters
-		devise_parameter_sanitizer.permit(:sign_up, :keys[:name_first,:name_last,:phone])
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:name_first, :name_last, :name_first_phonetic, :name_last_phonetic, :post_code, :email, :address, :phone, :is_quit])
+		devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :remember_created_at])
 	end
 end
