f19f78aa830fc0e00bb6f22e6e3ab97cacf330bf
渡邊義優 <yoshi4403@watanabeyoshiyuunoMacBook-Air.local>
2019-07-14 15:31:45 +0900

    tom

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,5 @@ class User < ApplicationRecord
          has_many :cart_items
          has_many :orders
          has_many :shipping_addresses
-         def active_for_authentication?
-           super && self.is_quit?
-         end
+
 end
