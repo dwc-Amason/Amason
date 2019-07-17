3ff85dfa6f50b89af125ef79c95ce6485329eea0
渡邊義優 <yoshi4403@watanabeyoshiyuunoMacBook-Air.local>
2019-07-17 19:40:00 +0900

    tom

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,10 +1,16 @@
 class CartItemsController < ApplicationController
     def index
     @cart_items = current_user.cart_items
+
     end
 
     def create
-    @cart_item = CartItem.new
-    @cart_item.save
+	    @cart_items = CartItem.new(cart_item_params)
+    	@cart_items.save
     end
 end
+
+private
+	def cart_item_params
+	   	params.require(:cart_item).permit(:id, :number, :user_id, :item_id)
+	end
