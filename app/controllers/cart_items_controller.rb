55578cf27e8164536d3b4b4fd61e161356920ecf
takaratomoya <tomoya48698103@gmail.com>
2019-07-17 14:25:24 +0900

    chris

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,6 +1,9 @@
 class CartItemsController < ApplicationController
 	def index
 		@cart_items = current_cart.cart_items
-		@user = User.find(params[:id])
+	end
+
+	def show
+		@user = current_user
 	end
 end
