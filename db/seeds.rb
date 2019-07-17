d73af99295646b95b37f7e45dedd79781269f136
Shodai Shibano <nameless.liberty.382@gmail.com>
2019-07-17 15:19:45 +0900

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,16 +20,29 @@
   )
 end
 
-#item
+#artist
 [
-  ['test1@test.com', '111111', '太郎', '田中', 'たろう', 'たなか', '111-1111', '東京', '111-1111-1111'],
-  ['test2@test.com', '222222', '知弥', '高良', 'ともや', 'たから', '222-2222', '沖縄', '222-2222-2222'],
-  ['test3@test.com', '333333', '神', '犬', 'がみ', 'いぬ', '333-3333', '霊界', '333-3333-3333'],
-  ['test4@test.com', '444444', '足', '頭', 'あし', 'あたま', '444-4444', '体', '444-4444-4444']
-].each do |mail, pass, name_f, name_l, name_f_1, name_l_1, post, add, num|
-  User.create!(
-    { email: mail, password: pass, name_first: name_f, name_last: name_l, name_first_phonetic: name_f_1,
-    	name_last_phonetic: name_l_1, post_code: post, address: add, phone: num }
+  ['Takara']
+].each do |name|
+  Artist.create!(
+    { name: name }
   )
 end
 
+#label
+[
+  ['Takara_comp']
+].each do |name|
+  Label.create!(
+    { name: name }
+  )
+end
+
+#genre
+[
+  ['J-POP']
+].each do |name|
+  Genre.create!(
+    { name: name }
+  )
+end
