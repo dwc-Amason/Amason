767dfe14a3d4f2891ffa07b4d12e562fe0e497c9
Shodai Shibano <51281791+Shodai-myv@users.noreply.github.com>
2019-07-20 23:06:08 +0900

    fix user.rb
    find mistake.
    I fixed it.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          has_many :cart_items
          has_many :orders
-         has_many :shipping_addresses
+         has_many :shippingAddresses
 
          acts_as_paranoid
 
